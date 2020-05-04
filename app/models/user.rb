e7cd5449282ac564ebae8a2ad61cfd7fe23e6136
natsumiko000 <k.natsu5454@gmail.com>
2020-05-04 11:28:05 +0000

    Userモデルにカラムを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+	enum status: {active:0, inactive:1}
+
+	# 物理削除の代わりにユーザーの`deleted_at`をタイムスタンプで更新
+  def soft_delete
+    update_attribute(:deleted_at, Time.current)
+    update_attributes(status: 1)
+  end
+
+  # ユーザーのアカウントが有効であることを確認
+  def active_for_authentication?
+    super && !deleted_at
+  end
+
+  # 削除したユーザーにカスタムメッセージを追加します
+  def inactive_message
+    !deleted_at ? super : :deleted_account
+  end
 end
