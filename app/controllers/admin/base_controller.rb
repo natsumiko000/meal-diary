a5f686c43ba22eec54f34ba2e033227246d07da5
natsumiko000 <k.natsu5454@gmail.com>
2020-05-04 12:09:54 +0000

    admin/userのcontrollersとviewsのファイルを生成

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,11 @@
+class Admin::BaseController < ApplicationController
+  add_flash_types :success, :info, :warning, :danger
+  before_action :admin_user
+  layout 'admin'
+
+  private
+
+  def admin_user
+    redirect_to(root_url) unless current_host && current_host.admin?
+  end
+end
