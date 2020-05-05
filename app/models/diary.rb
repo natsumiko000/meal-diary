d1b201328f861f1bb3f2b05325d95ba175980388
natsumiko000 <k.natsu5454@gmail.com>
2020-05-05 11:46:15 +0000

    migrateファイル編集、Eventモデルs作成

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,10 +1,10 @@
 class Diary < ApplicationRecord
 	belongs_to :user
+	has_one :event
 	has_many :diary_categories
 	has_many :categories, through: :diary_categories
 	has_many :diary_comments, dependent: :destroy
 
 	validates :user_id, :status, :category_id, presence: true
-
 	enum status: {public:0, private:1}
 end
--- db/migrate/20200502065023_init.rb
+++ db/migrate/20200502065023_init.rb
@@ -1,45 +1,52 @@
 class Init < ActiveRecord::Migration[5.2]
 	def change
-		create_table :diaries do |t|
+		create_table :events do |t|
 			t.integer :user_id,                     null: false
-			t.integer :category_id,        		    null: false
-			t.string :b_image_id,                   null: false
-			t.string :l_image_id,                   null: false
-			t.string :d_image_id,                   null: false
-			t.integer :weight,                      null: false
+			t.integer :diary_id,                    null: false
+
+			t.timestamps
+		end
+
+		create_table :diaries do |t|
+			t.integer :event_id,                    null: false
 			t.integer :status,                      null: false, default: 0
+			t.date :date,                           null: false
+			t.string :b_image_id
+			t.string :l_image_id
+			t.string :d_image_id
+			t.float :weight
 
 			t.timestamps
 		end
 
 		create_table :diary_comments do |t|
 			t.integer :user_id,                     null: false
-			t.integer :diary_id,         		    null: false
-			t.text :comment,						null: false
+			t.integer :diary_id,                    null: false
+			t.text :comment,                        null: false
 
 			t.timestamps
 		end
 
 		create_table :categories do |t|
-			t.string :name,							null: false
-			t.string :name_en,						null: false
-			t.integer :food_id,         		    null: false
+			t.string :name,                         null: false
+			t.string :name_en,                      null: false
+			t.integer :food_id,                     null: false
 			t.string :image_id,                     null: false
 
 			t.timestamps
 		end
 
 		create_table :diary_categories do |t|
-			t.integer :diary_id,         		    null: false
-			t.integer :category_id,      		    null: false
+			t.integer :diary_id,                    null: false
+			t.integer :category_id,                 null: false
 
 			t.timestamps
 		end
 
 		create_table :foods do |t|
-			t.string :name,							null: false
-			t.text :caption,						null: false
-			t.integer :category_id,      		    null: false
+			t.string :name,                         null: false
+			t.text :caption,                        null: false
+			t.integer :category_id,                 null: false
 			t.string :image_id,                     null: false
 
 			t.timestamps
