3228201349dbc8b4054ca0e95744cfa8b8952a21
natsumiko000 <k.natsu5454@gmail.com>
2020-05-06 14:37:25 +0000

    users#updateエラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,9 @@ class UsersController < ApplicationController
 	end
 
 	def update
-		@customer = Customer.find(params[:id])
-  		if @customer.update(customer_params)
-  			redirect_to admin_customer_path(@customer)
+		@user = User.find(params[:id])
+  		if @user.update(user_params)
+  			redirect_to user_path(@user), notice: 'You have updated user successfully.' 
   		else
   			render :edit
   		end
