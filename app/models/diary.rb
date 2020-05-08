a68b5355fb7164c8d368f8f99f20a00c12ad9208
natsumiko000 <k.natsu5454@gmail.com>
2020-05-08 12:03:43 +0000

    diaryの入力項目にMEMOを追加

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,10 +1,8 @@
 class Diary < ApplicationRecord
 	belongs_to :user
-	has_one :comment
 	has_one :event
 	has_many :diary_categories
 	has_many :categories, through: :diary_categories
-	has_many :diary_comments, dependent: :destroy
 	has_one_attached :b_image
 	has_one_attached :l_image
 	has_one_attached :d_image
