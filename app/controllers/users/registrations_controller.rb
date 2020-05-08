a84cbffef10c21bfc114e977f02f6c22fd9e9b4b
natsumiko000 <k.natsu5454@gmail.com>
2020-05-08 12:40:55 +0000

    sign_upエラー修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -66,4 +66,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  # 
+  # protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
