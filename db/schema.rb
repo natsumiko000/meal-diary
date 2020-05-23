3fa4e477f52faf532e0b59570e1b3a8d359182c0
natsumiko000 <k.natsu5454@gmail.com>
2020-05-23 12:51:50 +0000

    お問合せ機能作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_123647) do
+ActiveRecord::Schema.define(version: 2020_05_23_122457) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -76,6 +76,13 @@ ActiveRecord::Schema.define(version: 2020_05_05_123647) do
     t.string "name", null: false
     t.text "caption", null: false
     t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inquiries", force: :cascade do |t|
+    t.string "name"
+    t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
