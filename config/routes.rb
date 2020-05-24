618476080688e836ffdcbd404526519e8da51f6d
natsumiko000 <k.natsu5454@gmail.com>
2020-05-24 08:42:45 +0000

    admins/home/topレイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 	get 'home/about'
   	
 	devise_for :admins, controllers: {
-		sessions:      'admins/sessions',
+		sessions: 'admins/sessions',
 		passwords:     'admins/passwords',
 		registrations: 'admins/registrations'
 	}
