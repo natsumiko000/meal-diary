fcf57ffa8a791ecc8227210ad3b5432f8d86f3cc
natsumiko000 <k.natsu5454@gmail.com>
2020-05-27 12:28:01 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -14,33 +14,28 @@ Rails.application.routes.draw do
 		registrations: 'users/registrations'
 	}
 
-	resources :users, only: [:index, :show, :edit, :update] do
+	resources :users, only: [:show, :edit, :update] do
 		member do
 			get 'confirm'
 		end
 	end
-	resources :foods, only: [:index, :show]
-	resources :diary_categories
-	resources :diaries do
+	resources :diaries, only: [:create, :update, :destroy] do
 		member do
 			get 'data'
 		end
 		collection do
 			get 'record'
 		end
-		resource :diary_comments, only: [:create, :destroy]
 	end
 	resources :categories, only: [:index, :show]
 	resources :inquiries, only: [:new, :create]
 
 	namespace :admins do
 		get 'home/top'
-		resources :users
-		resources :foods
-		resources :diary_categories
-		resources :diaries
-		resources :diary_comments
-		resources :categories
-		resources :inquiries
+		resources :users, only: [:index]
+		resources :foods[:index, :new, :create, :edit, :update, :destroy]
+		resources :diaries[:index, :show, :edit, :update, :destroy]
+		resources :categories[:index, :new, :create, :edit, :update, :destroy]
+		resources :inquiries[:index, :show, :destroy]
 	end
 end
