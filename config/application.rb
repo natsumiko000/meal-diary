4c574d99578d8c11fc440dc8f8711abae39bb292
natsumiko000 <k.natsu5454@gmail.com>
2020-05-28 15:08:00 +0000

    fix-language-to-japanese

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,8 @@ module MealDiary
   class Application < Rails::Application
     config.load_defaults 5.2
 	
-    config.i18n.default_locale = :ja
-    config.i18n.enforce_available_locales = true
-    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+	config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '**', '*.{rb,yml}').to_s]
     config.time_zone = 'Asia/Tokyo'
+	config.i18n.default_locale = :ja
   end
 end
